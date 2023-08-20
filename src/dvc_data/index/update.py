1c1b36780a935d1f468dfac7f3aa7a2aa30f488d
Ruslan Kuprieiev <kupruser@gmail.com>
2023-08-21 01:25:07 +0300

    index: add basic update_meta

--- src/dvc_data/index/update.py
+++ src/dvc_data/index/update.py
@@ -1,6 +1,7 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List, Optional
 
 from .diff import UNCHANGED, diff
+from .index import Meta
 
 if TYPE_CHECKING:
     from .index import BaseDataIndex, DataIndex
@@ -10,3 +11,43 @@ def update(new: "DataIndex", old: "BaseDataIndex") -> None:
     for change in diff(old, new, with_unchanged=True, meta_only=True):
         if change.typ == UNCHANGED:
             change.new.hash_info = change.old.hash_info
+
+
+def update_meta(index: "BaseDataIndex", storages: Optional[List[str]] = None):
+
+    if storages is None:
+        storages = ["cache"]
+
+    updates = []
+    for key, entry in index.iteritems():
+        if entry.meta and entry.meta.isdir:
+            continue
+
+        storage_info = index.storage_map.get(entry.key)
+        if not storage_info:
+            continue
+
+        for name in storages:
+            storage = getattr(storage_info, name, None)
+            if not storage:
+                continue
+
+            data = storage.get(entry)
+            if not data:
+                continue
+
+            fs, fs_path = data
+            try:
+                info = fs.info(fs_path)
+            except FileNotFoundError:
+                continue
+            meta = Meta.from_info(info, fs.protocol)
+
+            updates.append((key, meta))
+
+    for key, meta in updates:
+        entry = index[key]
+        entry.meta = meta
+        index[key] = entry
+
+    index.commit()
